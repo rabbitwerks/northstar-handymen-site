43c38bd2bef9a3f7c04aed749d82e9e2ad6318c1
SEAN_RABBITWERKS <rabbitwerks.js@gmail.com>
2020-08-25 00:08:34 -0500

    init project, added a landing view, collected img and color assets

---
+++ src/views/Landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="landing--outer">
+    <div class="hero-image-container">
+
+    </div>
+    <div class="welcome-text">
+      Northstar Handymen
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
+
